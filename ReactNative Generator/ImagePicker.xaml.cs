e1b9fb1a2fb039b47b2b1107e976cb28d05695b9
LenyaPlay <lenyaplay@mail.ru>
2024-01-15 09:54:33 +0300

    upd

---
+++ ReactNative Generator/ImagePicker.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
+using Path = System.IO.Path;
+
+namespace ReactNative_Generator
+{
+    /// <summary>
+    /// Interaction logic for ImagePicker.xaml
+    /// </summary>
+    public partial class ImagePicker : Window, INotifyPropertyChanged
+    {
+        public ObservableCollection<ImageSource> imageSource { get; set; } = new ObservableCollection<ImageSource>();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public Dictionary<ImageSource, String> paths = new Dictionary<ImageSource, String>();
+
+        public ImagePicker()
+        {
+            InitializeComponent();
+
+
+            DataContext = this;
+        }
+
+
+        private void StackPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            (sender as Control).Background = new SolidColorBrush(Colors.Red);
+           
+        }
+
+        private void imagesListBox_DragLeave(object sender, DragEventArgs e)
+        {
+            (sender as Control).Background = new SolidColorBrush();
+        }
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+            var data = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if(data == null || data.Length == 0 )
+                return;
+
+            imageSource.Clear();
+
+            foreach ( var item in data )
+            {
+                if (Directory.Exists(item))
+                    LoadImages(Directory.GetFiles(item));
+
+                if (File.Exists(item))
+                    LoadImage(item);
+    
+            }
+
+            //imageSource = new ObservableCollection<ImageSource>();
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(imageSource)));
+            (sender as Control).Background = new SolidColorBrush();
+        }
+
+        private void LoadImages(string[] paths)
+        {
+            foreach (var path in paths)
+                if (File.Exists(path))
+                    LoadImage(path);
+
+        }
+
+        private void LoadImage(string path)
+        {
+            try
+            {
+                var src = new BitmapImage(new Uri(path, UriKind.Absolute));
+                imageSource.Add(src);
+
+                paths.Add(src, path);
+
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = (sender as Image);
+
+            var src = img.Source;
+
+            if (!paths.ContainsKey(src))
+                return;
+
+            String path = paths[src];
+            String fileName= Path.GetFileName(path);
+
+            path = path.Remove(path.LastIndexOf(Path.DirectorySeparatorChar));
+            path = path.Remove(0, path.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+
+            String rnPath = $"./{fileName}";
+
+            String aspectRatio = (src.Width / src.Height).ToString().Replace(",", ".");
+            String imgComp = $"<Image source={{require('{rnPath}')}} style={{{{width: '100%', height: undefined, aspectRatio: {aspectRatio} }}}}/>";
+                ;
+            DragDrop.DoDragDrop(img, imgComp, DragDropEffects.Copy);
+        }
+
+        private void GroupBox_Drop(object sender, DragEventArgs e)
+        {
+            var data = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var item in data)
+                RenameFilesInDirectory(item, new Random().Next(5, 12));
+        }
+
+        static void RenameFilesInDirectory(string directoryPath, int nameLength)
+        {
+            Random random = new Random();
+
+            // Check if the directory exists
+            if (Directory.Exists(directoryPath))
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+
+                foreach (string filePath in files)
+                {
+                    string extension = Path.GetExtension(filePath);
+                    string randomName = GenerateRandomName(random, nameLength);
+
+                    string newFilePath = Path.Combine(directoryPath, randomName + extension);
+
+                    // Ensure the new filename doesn't already exist
+                    while (File.Exists(newFilePath))
+                    {
+                        randomName = GenerateRandomName(random, nameLength);
+                        newFilePath = Path.Combine(directoryPath, randomName + extension);
+                    }
+
+                    File.Move(filePath, newFilePath);
+                    Console.WriteLine($"Renamed '{Path.GetFileName(filePath)}' to '{Path.GetFileName(newFilePath)}'");
+                }
+
+                Console.WriteLine("All files renamed successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Directory '{directoryPath}' does not exist.");
+            }
+        }
+
+        static string GenerateRandomName(Random random, int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            char[] name = new char[length];
+
+            name[0] = chars[random.Next(chars.Length - 10)]; //First character is no digital
+            for (int i = 1; i < length; i++)
+            {
+                name[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(name);
+        }
+    }
+}
---
+++ ReactNative Generator/MainWindow.xaml.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+using System.Drawing;
+using System.IO;
+
+namespace ReactNative_Generator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Process process = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartCmd();
+
+            frame1.Content = new ImagePicker().Content;
+        }
+
+        private void StartCmd()
+        {
+            if (process != null) 
+                process.StandardInput.WriteLine("exit");
+
+            process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.OutputDataReceived += Process_OutputDataReceived;
+            
+            process.Start();
+            process.BeginOutputReadLine();
+
+        }
+
+        private void OnRunCommandClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string command = cmdInput.Text;
+                process.StandardInput.WriteLine(command);
+            }
+            catch (Exception ex)
+            {
+                outputTextBlock.Text = "Error running command: " + ex.Message;
+            }
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                outputTextBlock.Text += e.Data + "\n";
+                sv.ScrollToEnd();
+                Activate();
+            });
+        }
+
+        private void OnCloseCmdClick(object sender, RoutedEventArgs e)
+        {
+            process.StandardInput.WriteLine("exit");
+        }
+
+        private void OnStartCmdClick(object sender, RoutedEventArgs e)
+        {
+            StartCmd();
+        }
+
+        private void OnImagesFunctionClick(object sender, RoutedEventArgs e)
+        {
+            string initialDirectory = @"C:\";
+
+            // Show the image open dialog and get the selected image file(s)
+            string[] selectedImageFiles = ShowImageOpenDialog(initialDirectory);
+
+            // Generate the JavaScript function with the given template and selected images
+            string jsFunction = GenerateJsFunction(selectedImageFiles);
+
+            // Output the JavaScript function
+            BigTextBox.Text = jsFunction;
+            Console.WriteLine(jsFunction);
+        }
+
+        static string[] ShowImageOpenDialog(string initialDirectory)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+                // Set the initial directory for the dialog
+                openFileDialog.InitialDirectory = initialDirectory;
+
+                // Allow selecting multiple files
+                openFileDialog.Multiselect = true;
+
+            // Filter only PNG files (you can customize this filter if needed)
+            openFileDialog.Filter = "Image files|*.png;*.jpg;*.jpeg;*.bmp;*.gif;*.tiff;*.ico|PNG files (*.png)|*.png|JPEG files (*.jpg;*.jpeg)|*.jpg;*.jpeg|BMP files (*.bmp)|*.bmp|GIF files (*.gif)|*.gif|TIFF files (*.tiff)|*.tiff|Icon files (*.ico)|*.ico|All files (*.*)|*.*";
+
+
+            if (openFileDialog.ShowDialog().GetValueOrDefault())
+                {
+                    // Return the selected image file(s)
+
+                    return openFileDialog.FileNames;
+                }
+
+
+            // Return an empty array if the dialog was canceled or closed
+            return new string[0];
+        }
+
+        static string GenerateJsFunction(string[] imageFiles)
+        {
+            string jsFunction = "const Img = ({num} : any) => {\n" +
+                               "    const images = [\n";
+
+            foreach (string imageFile in imageFiles)
+            {
+
+                BitmapImage image = new BitmapImage(new Uri(imageFile));
+                string fileName = Path.GetFileName(imageFile); // Get the file name
+
+                jsFunction += $"        <Image source={{require('{"../images/" +fileName}')}}   style={{{{width: '100%', height: undefined, aspectRatio: {image.PixelWidth}/{image.PixelHeight}}}}} />,\n";
+            }
+
+            jsFunction += "    ];\n" +
+                          "    return images[num-1];\n" +
+                          "}\n";
+
+            Clipboard.SetText(jsFunction);
+
+            return jsFunction;
+        }
+
+        static double GetImageAspectRatio(string imagePath)
+        {
+            BitmapImage image = new BitmapImage(new Uri(imagePath));
+            double aspectRatio = (double)image.PixelWidth / image.PixelHeight;
+            return aspectRatio;
+        }
+
+        private void OnGetTextComponentsClick(object sender, RoutedEventArgs e)
+        {
+            string s = Clipboard.GetText();
+
+            string[] strs = s.Split('\n');
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (string str in strs)
+            {
+                if(str=="")
+                    continue; 
+                stringBuilder.Append($"<Text style={{{{...styles.text}}}}>{str.Remove(str.Length-1,1)}</Text>\n");
+            }
+
+            //for remove last \n
+           // stringBuilder.Remove(stringBuilder.Length - 1, 1); 
+            BigTextBox.Text = stringBuilder.ToString();
+            Clipboard.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Clipboard.SetText(stringBuilder.ToString());
+                    return;
+                }
+                catch { }
+                System.Threading.Thread.Sleep(10);
+            }
+
+        }
+    }
+}
